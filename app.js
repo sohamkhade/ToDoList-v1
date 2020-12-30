2ba02bcc99f66bbccdd88edd5d9bb453a49d89f1
Soham Khade <sohamkhade@Sohams-MacBook-Pro.local>
2020-12-30 10:15:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const date = require(__dirname+'/date.js'); //inbuilt export or module
+
+const app = express();
+
+var items = [];
+var workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+
+    var day = date.getDate();
+
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {listTitle: "Work", newListItems: workItems});
+});
+
+app.get("/about",function(req,res){
+   res.render("about");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
